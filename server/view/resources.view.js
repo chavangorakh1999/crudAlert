9e04aff024a1e3d1b1a7ea321309d438a508d320
chavangorakh1999 <chavangorakh19999@gmail.com>
2023-03-14 19:06:24 +0530

    Added pagination

--- server/view/resources.view.js
+++ server/view/resources.view.js
@@ -13,18 +13,45 @@ var success_status = response_code.success().status;
 
 module.exports= {
     readResource(req,res){
-            Resources_model.find({})
-            .then((resource)=>{
-                response_code.message="Resource fetched successfully";
-                response_code.data=resource;
-                global.io.emit('message', 'Resource fetched successfully');
-                res.status(success_status).send(response_code.success());
-            })
-            .catch((err)=>{
-                response_code.error = {
-                    'message':{...err}
-                }
-                res.status(server_status).send(response_code.serverError());
-            })
+        let page=req.body?.page;
+        let limit=req.body?.limit;
+
+        let data= {
+            page,
+            limit
+        }
+        let rules = {
+            page:'required|numeric',
+            limit:'required|numeric'
+        }
+    
+        let validation = new Validator(data, rules);
+    
+        if(!validation.passes()){
+            response_code.error = {
+                'message':{...validation.errors.errors}
+            }
+            res.status(request_status).send(response_code.badRequest());
+        }else{
+            page=Number(page);
+            limit=Number(limit);
+            Resources_model.find({},{},{
+                    skip: ((limit * page) - limit),
+                    limit: limit
+                })
+                .then((resource)=>{
+                    response_code.message="Resource fetched successfully";
+                    response_code.data=resource;
+                    global.io.emit('message', 'Resource fetched successfully');
+                    res.status(success_status).send(response_code.success());
+                })
+                .catch((err)=>{
+                    response_code.error = {
+                        'message':{...err}
+                    }
+                    res.status(server_status).send(response_code.serverError());
+                })
+        }
+        
     }
 }
